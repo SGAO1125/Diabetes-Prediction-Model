38ef0d6f667f0450b3962a67bcce52465877a7c3
SGAO1125 <160781453+SGAO1125@users.noreply.github.com>
2025-05-11 18:27:38 +0000

    Customer

---
+++ Customer.java
@@ -0,0 +1,29 @@
+public class Customer{
+    private String UserID;
+    private String Name;
+    private String[] data;
+    
+    public Customer(String id, String name, String input[]){
+        UserID = id;
+        Name = name;
+        data = input;
+    }
+    public String getUserID(){
+        return UserID;
+    }
+
+    public String getName(){
+        return Name;
+    }
+    public String[] getdata(){
+        return data;
+    }
+
+
+    public static void main(String args[]){
+        System.out.println("Hello World!!");
+        String inputforSteven [] = {"han", "due", "Trhee"};
+        Customer Steven = new Customer("21232324","Steven", inputforSteven);
+        System.out.println("The id of the user " + Steven.Name + " is: " +  Steven.UserID);
+    }
+}
