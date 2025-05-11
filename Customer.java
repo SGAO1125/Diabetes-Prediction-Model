1088292dde1a79a0d8201a57600bae99df1cd6cc
SGAO1125 <160781453+SGAO1125@users.noreply.github.com>
2025-05-11 20:48:26 +0000

    Customer

--- Customer.java
+++ Customer.java
@@ -1,10 +1,11 @@
+import java.util.UUID;
 public class Customer{
     private String UserID;
     private String Name;
     private String[] data;
     
-    public Customer(String id, String name, String input[]){
-        UserID = id;
+    public Customer(String name, String input[]){
+        UserID = UUID.randomUUID().toString().substring(0, 12);
         Name = name;
         data = input;
     }
@@ -19,11 +20,7 @@ public class Customer{
         return data;
     }
 
-
     public static void main(String args[]){
-        System.out.println("Hello World!!");
-        String inputforSteven [] = {"han", "due", "Trhee"};
-        Customer Steven = new Customer("21232324","Steven", inputforSteven);
-        System.out.println("The id of the user " + Steven.Name + " is:" +  Steven.UserID);
+    
     }
 }
