05fdeac5ccdbe3acb81bf2b3827cdae37f46ac44
Hao are you <158393369+HaoRyou@users.noreply.github.com>
2025-05-12 21:39:16 -0400

    Update CustomizeModel.java

--- CustomizeModel.java
+++ CustomizeModel.java
@@ -42,18 +42,3 @@ public class CustomizeModel{
         return customizedDataset;
     }
 }
-=======
-public class CustomizeModel {
-        String[] Dataset;
-    
-    public CustomizeModel(){
-    }
-    void CustomizeModelDataset(){
-        
-    }
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
