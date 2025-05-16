c01b52e786783ffd318e3474b7e9fdaa3e7b83cc
Leo-Parkz <161254218+Leo-Parkz@users.noreply.github.com>
2025-05-15 20:54:02 -0400

    Add files via upload

--- Diabetes/Customer.java
+++ Diabetes/Customer.java
@@ -1,22 +1,23 @@
-import java.util.UUID;
+import java.util.UUID; // Import UUID class which is used to generate unique ID
 public class Customer{
-    private String UserID;
-    private String Name;
-    private String[] data;
+    private String UserID; // Unique ID for each customer 
+    private String Name; // Customers name
+    private String[] data; // Array to store customer's data
     
+    // Initialize UserID, Name, and data
     public Customer(String name, String input[]){
         UserID = UUID.randomUUID().toString().substring(0, 12);
         Name = name;
         data = input;
     }
-    public String getUserID(){
+    public String getUserID(){ // Getter for UserID
         return UserID;
     }
 
-    public String getName(){
+    public String getName(){ // Getter for Name
         return Name;
     }
-    public String[] getdata(){
+    public String[] getdata(){ // Getter for data array
         return data;
     }
 
