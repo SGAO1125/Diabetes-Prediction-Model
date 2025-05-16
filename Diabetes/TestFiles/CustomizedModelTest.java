029c07db9f2b2c43ee8bb7490375994ff444282b
SGAO1125 <160781453+SGAO1125@users.noreply.github.com>
2025-05-15 23:11:42 -0400

    Update CustomizedModelTest.java

--- Diabetes/TestFiles/CustomizedModelTest.java
+++ Diabetes/TestFiles/CustomizedModelTest.java
@@ -3,7 +3,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class CustomizedModelTest {
+public class CustomizeModelTest {
     private static final List<String> TEST_DATA = Arrays.asList( //testing data
             "Female,80.0,25.19,140,6.6,1,0,0",
             "Female,54.0,27.32,80,6.6,0,0,0",
@@ -24,14 +24,22 @@ public class CustomizedModelTest {
                 "Female,80.0,25.19,140,6.6,1,0,0",
                 "Female,54.0,27.32,80,6.6,0,0,0",
                 "Male,28.0,27.32,158,5.7,0,0,0",
+                "Female,36.0,23.45,155,5.0,0,0,0",
+                "Male,76.0,20.14,155,4.8,1,1,0",
+                "Female,35.0,24.89,158,5.1,0,0,0",
                 "Female,66.0,26.51,155,6.2,0,0,0",
                 "Female,36.0,35.24,126,6.5,0,0,1",
-                "Female,35.0,24.89,158,5.1,0,0,0",
-                "Female,39.0,24.23,140,5.0,0,0,0"
+                "Female,39.0,24.23,140,5.0,0,0,0",
+                "Male,40.0,23.88,90,4.8,0,0,0"
         );
 
         CustomizeModel model = new CustomizeModel(TEST_DATA, 6.0f);
         List<String> actualResults = model.getCustomizedDataset();
+
+        System.out.println("Expected Results:");
+        expectedResults.forEach(System.out::println);
+        System.out.println("Actual Results:");
+        actualResults.forEach(System.out::println);
 
         assertEquals(expectedResults, actualResults);
     }
@@ -49,6 +57,13 @@ public class CustomizedModelTest {
 
         CustomizeModel model = new CustomizeModel(TEST_DATA, 3.5f);
         List<String> actualResults = model.getCustomizedDataset();
+        // Print Expected Results
+        System.out.println("Expected Results:");
+        expectedResults.forEach(System.out::println);
+
+        // Print Actual Results
+        System.out.println("Actual Results:");
+        actualResults.forEach(System.out::println);
 
         assertEquals(expectedResults, actualResults);
     }
@@ -63,6 +78,8 @@ public class CustomizedModelTest {
         CustomizeModel model = new CustomizeModel(TEST_DATA, 9.0f);
         List<String> actualResults = model.getCustomizedDataset();
 
+        System.out.println("Expected Results: " + expectedResults);
+        System.out.println("Actual Results: " + actualResults);
+
         assertEquals(expectedResults, actualResults);
     }
-}
